e4bb05b77e8dc22d12d050cb932ee59f6b22a658
Bianca León <bleon@usm.cl>
2025-05-26 23:17:17 -0400

    refactor: hacer abstracta la clase Component y agregar método getView()

--- Etapa 1/Component.java
+++ Etapa 1/Component.java
@@ -1,15 +1,26 @@
-public class Component {
+import javafx.scene.Node;
+
+//lo declaramos abstracta porque no representa un componente completo por si sola
+//solo sirve como base para subclases que implementa getView()
+
+public abstract class Component {
    protected Component (){}  // to ban creation of publisher or subscriber without name.
+
+
    public Component(String componentName, String topicName){
-      name=componentName;
+      name = componentName;
       this.topicName = topicName;
    }
+
    public String getName(){
       return name;
    }
+
    public String getTopicName(){
       return topicName;
    }
+   public abstract Node getView();
+
    protected String name;
    protected String topicName;
 }
