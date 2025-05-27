f43615ad69b604276f9de5e7144edf65b24cc376
bleonkaori <163612493+bleonkaori@users.noreply.github.com>
2025-05-26 23:50:38 -0400

    feat: agregar botón en VideoFollower que muestra el último link publicado

--- Etapa 1/VideoFollower.java
+++ Etapa 1/VideoFollower.java
@@ -1,18 +1,31 @@
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.Node;
+
+//etapa 2 boton q permite reproducir video, en la 1 solo lo muestra
 
 public class VideoFollower extends Subscriber {
-   public VideoFollower(String name, String topicName) {
-      super(name, topicName);
-      //??
-   }
-   public void update(String message) {
-      //??
-   }
-   public HBox getView() {
-      return view;
-   }
    private HBox view;
    private Button video;
+
+   public VideoFollower(String name, String topicName) {
+      super(name, topicName);
+      video = new Button(""); //Se crea un boton vacio
+      Label info = new Label(" "+topicName + " -> " + name + ":");
+      view = new HBox(10,info, video);
+
+
+   }
+
+   @Override
+   public void update(String message) {
+      video.setText(message); //Aqui es para actualizar con el nuevo link
+   }
+   @Override
+   public Node getView(){
+      return view;
+   }
+
+
 }
